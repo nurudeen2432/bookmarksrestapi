b6c25fbbcc4933dfbb1238ba33e6c408efeab9f4
Nurudeen Durowade <Nurudeen.durowade@wemabank.com>
2025-01-10 18:21:03 +0100

    Created our application entry point app.py

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-from flask import Flask;
+#This imports the Flask class from the flask
+from flask import Flask, jsonify
 
+# creates an instance of the Flask class.
+app = Flask(__name__)
+
+#route decorator using app.get instead of app.route
+@app.get("/")
+def index():
+    return "Hello World"
+
+
+
+@app.get("/hello")
+def hello():
+    return jsonify({"message":"Hello world"})
+
+
+# if(__name__) == '__main__':
+#              app.run()
